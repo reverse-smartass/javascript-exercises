817caf6fe21ae33c8a89183ee98434a8994ce3bb
reverse-smartass <nguyenminhdore@gmail.com>
2026-02-02 15:43:11 -0500

    m

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,15 @@
-const factorial = function() {
+const factorial = function(i) {
 
+    if(typeof i !== 'number' || i % 1 !== 0 || i < 0){
+        return undefined;
+    }
+
+
+    if(i === 1 || i === 0){
+        return 1;
+    }
+
+    return factorial(i-1)*i;
 };
 
 // Do not edit below this line
--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,43 @@
-const contains = function() {
-  
+const contains = function (object, target) {
+  if (target === null) return false;
+  for (const [key, value] of Object.entries(object)) {
+    if (typeof value === "object" && value !== null && value !== undefined) {
+      //console.log(key + " ");
+      if (typeof target === "object") {
+        if (target === value){
+          return true;
+        }
+      }
+      if (contains(value, target)) {
+        return true;
+      }
+    } else {
+      //console.log(key + " " + value);
+
+      if (
+        value === target ||
+        (typeof target === "number" && isNaN(target) && isNaN(value))
+      ) {
+        //console.log(true);
+        return true;
+      }
+    }
+  }
+  return false;
 };
-  
+
+function compareArrays(arr1, arr2){
+    if(arr1.length != arr2.length){
+        return false
+    }else{
+        for(let i = 0; i < arr1.length; i++){
+            if(arr1[i] !== arr2[i]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 // Do not edit below this line
 module.exports = contains;
